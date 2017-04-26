a9add2016fb192d776d4fc7c96a9f80b24478fb5
ratsilva <ricardosilva@sistemaids.com.brgit config --global user.name Ricardo Silvagit config --global user.email ricardosilva@sistemaids.com.br>
2017-04-26 20:38:18 -0300

    Heroic problems #3
    Tentando corrigir problemas com o heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,12 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
+  config.cache_classes = true
+config.serve_static_files = true
+config.assets.compile = true
+config.assets.digest = true
+
+
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
