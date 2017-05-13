7ac99533c96aa8730ce2ed8a1a3b676e84296e52
ratsilva <ricardosilva@sistemaids.com.brgit config --global user.name Ricardo Silvagit config --global user.email ricardosilva@sistemaids.com.br>
2017-05-13 15:53:58 -0300

    Template fixing bugs
    - Desacoplando cabeçalho, menu lateral e rodapé do conteúdo da pagina.
    - Corrigindo caminho relativo de imagens, css e javascript
    - Limpando conteúdos desnecessários do template
    - Pre-compilando arquivos .css e .js

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,4 @@ Rails.application.routes.draw do
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "home#index"
 
-  get "home/media_gallery"
-
 end
