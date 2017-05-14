386a726bb1ac15f4724a78c44556f8f84317f03e
pedro <pf.cassiano@gmail.com>
2017-05-13 22:04:40 -0300

    usando o template no aplication.html.erb

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,16 @@ ActiveRecord::Schema.define(version: 20170510021106) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "projects", force: :cascade do |t|
+    t.string   "name"
+    t.date     "date_begin"
+    t.date     "date_end"
+    t.decimal  "cost"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -31,16 +41,6 @@ ActiveRecord::Schema.define(version: 20170510021106) do
     t.string   "name"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  end
-  
-  create_table "projects", force: :cascade do |t|
-    t.string   "name"
-    t.date     "date_begin"
-    t.date     "date_end"
-    t.decimal  "cost"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
   end
 
 end
