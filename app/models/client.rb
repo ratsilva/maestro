6bb25362dd827fdb5256a58493981accd52ea90c
ratsilva <ricardosilva@sistemaids.com.brgit config --global user.name Ricardo Silvagit config --global user.email ricardosilva@sistemaids.com.br>
2017-05-16 00:13:34 -0300

    Lista de Clientes - Pesquisar
    Implementando um campo de busca por nome e por email na lista de
    clientes

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,11 @@
 class Client < ApplicationRecord
+
+	def self.search(search)
+	    if search
+	      where(["UPPER(company_name) LIKE UPPER(?) OR UPPER(email) LIKE UPPER(?)","%#{search}%", "%#{search}%"])
+	    else
+	      all
+	    end
+  end
+
 end
