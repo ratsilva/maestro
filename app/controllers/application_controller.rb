294a6de529c5e7b84337555babaa7e39e38a4402
Pedro Henrique Farias Cassiano <pf.cassiano@gmail.com>
2017-05-22 13:57:22 -0300

    layout diferente para o devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,20 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  layout :layout_by_resource
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+  
+  private
+
+  def layout_by_resource
+    if devise_controller?
+      "homepage"
+    else
+      "application"
+    end
   end
 end
